5c4ff82ebe5c8ba411d116984fb2e8479fd755a1
LuckyMan26 <volikartem6@gmail.com>
2023-03-23 22:30:17 +0200

    Updated html and created task class

--- djangoApp/testproject/testapp/Task.py
+++ djangoApp/testproject/testapp/Task.py
@@ -1,2 +1,14 @@
+class Task:
+    task = None
+    answear = None
+    timestamp = None
+    points = None
+
+    def __init__(self):
+        self.task = "2 + 2"
+        self.answear = 4
+        points = 2
+
+
 
 
